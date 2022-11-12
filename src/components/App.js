0a3a11ecda2f664746839fc3e8d6824ab5e395a0
Surender Kumar <111198040+monudisodia@users.noreply.github.com>
2022-11-12 21:40:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import "../styles/App.css";
 
 const App = () => {
+  
+  const [data,setData] = useState(true);
+  
+  
+  
   return (
     <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
+       {  data?<div id="welcome-div"><h1>Welcome to Newton School!!</h1></div>
+       :
+      <div id="about-div"><h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+          opportunities.</h1></div>}
+      <button id="toggle" onClick={()=>setData(!data)}>Toggle</button>
     </div>
   );
 };
