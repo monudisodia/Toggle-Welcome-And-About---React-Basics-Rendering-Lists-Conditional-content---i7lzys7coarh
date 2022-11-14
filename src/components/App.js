dcf8cec9fd1271ce6b192d0cd9490fc0b2e16b8d
Surender Kumar <111198040+monudisodia@users.noreply.github.com>
2022-11-14 19:51:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,14 @@ const App = () => {
   
   return (
     <div id="main">
-       {  data?<div id="welcome-div"><h1>Welcome to Newton School!!</h1></div>
+       {  data?(<div id="welcome-div"><h1>Welcome to Newton School!!</h1></div>)
        :
-      <div id="about-div"><h1>
+      (<div id="about-div"><h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.</h1></div>}
       <button id="toggle" onClick={()=>setData(!data)}>Toggle</button>
-    </div>
+    </div>)
   );
 };
 
