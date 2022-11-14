4a3739c7a86f1aecd9ba28d0326b15b485b7046c
Surender Kumar <111198040+monudisodia@users.noreply.github.com>
2022-11-14 19:54:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import "../styles/App.css";
+import { useState } from "react";
 
 const App = () => {
-  
-  const [data,setData] = useState(true);
-  
-  
-  
+  const [div,setDiv] = useState(true);
+  const onclick = () =>{
+    if(div === true){
+      setDiv(false)
+    }else{
+      setDiv(true);
+    }
+  }
   return (
     <div id="main">
-       {  data?(<div id="welcome-div"><h1>Welcome to Newton School!!</h1></div>)
-       :
-      (<div id="about-div"><h1>
+      {div === true ?
+      (<div id="welcome-div">
+        <h1>Welcome to Newton School!!</h1>
+      </div>):
+      (<div id="about-div">
+        <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
-          opportunities.</h1></div>}
-      <button id="toggle" onClick={()=>setData(!data)}>Toggle</button>
-    </div>)
+          opportunities.
+        </h1>
+      </div>)}
+      <button id="toggle" onClick={onclick} >Toggle</button>
+    </div>
   );
 };
 
